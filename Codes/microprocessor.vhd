f94038a8d760c4ce590a0817dd6af27bbde1b991
Ashwin Wagh <waghashwin96@gmail.com>
2016-11-23 16:28:57 +0530

    Update microprocessor.vhd

--- Codes/microprocessor.vhd
+++ Codes/microprocessor.vhd
@@ -1,13 +1,3 @@
-
-
-
-
-
-
-
-
-
-
 library ieee;
 use ieee.std_logic_1164.all;
 
@@ -20,6 +10,28 @@ entity microprocessor is
 		  
 architecture mic of microprocessor is
 	signal pcIn;
+	signal pcPlusOne,pcPlusOneR;
+	signal instruction,instructionR;
+	signal pcPlusOneOut_d;
+	signal pcMux_crtl_d;
+	signal A_sel_d;
+	signal B_sel_d;
+	signal rf_dataIn_mux_d;
+	signal carryEnable_d;
+	signal zeroEnable_d;
+	signal signExt_d;
+	signal alu_a_muxCrtl_d;
+	signal alu_b_muxCrtl_d;
+	signal r7_enable_d;
+	signal memWrite_en_d;
+	signal beq_pc_crtl_d;
+	signal rf_wren_mux _d;
+	signal rf_wren_d;
+	signal counter_mux_d;
+	signal mem_mux_d;
+	signal rf_dataIn_sel_d;
+	signal alu_crtl_d;
+	signal op2in _d;	
 	
 begin
 
@@ -27,26 +39,26 @@ begin
 	IF_ID  : registers generic map() port map(input(31 downto 16) => instruction, input(15 downto 0) => pcPlusOne,clock => clock, reset => reset, enable =>, output(31 downto 16) => instructionR, output(15 downto 0) => pcPlusOneR);
 	
 	Decoded: decode port map(instruction  => instructionR,
-									clock			 => clock,
-									pcPlusOneIn  => pcPlusOneR,
-									pcPlusOneOut => pcPlusOneOut_d,
-									pcMux_crtl	 => pcMux_crtl_d,
-									A_sel			 => A_sel_d,
-									B_sel			 => B_sel_d,
-									rf_dataIn_mux=> rf_dataIn_mux_d,
-									carryEnable  =>
-									zeroEnable   : out std_logic;
-									signExt      : out std_logic_vector(15 downto 0);
-									alu_a_muxCrtl: out std_logic;
-									alu_b_muxCrtl: out std_logic_vector(1 downto 0);
-									r7_enable	 : out std_logic;
-									memWrite_en  : out std_logic;
-									beq_pc_crtl  : out std_logic;
-									rf_wren_mux  : out std_logic;
-									rf_wren		 : out std_logic;
-									counter_mux  : out std_logic;
-									mem_mux		 : out std_logic;
-									rf_dataIn_sel: out std_logic_vector(2 downto 0);
-									alu_crtl     : out std_logic_vector(1 downto 0);
-									op2in			 : out std_logic_vector(1 downto 0));
+				clock	     => clock,
+				pcPlusOneIn  => pcPlusOneR,
+				pcPlusOneOut => pcPlusOneOut_d,
+				pcMux_crtl	 => pcMux_crtl_d,
+				A_sel		 => A_sel_d,
+				B_sel		 => B_sel_d,
+				rf_dataIn_mux=> rf_dataIn_mux_d,
+				carryEnable  => carryEnable_d,
+				zeroEnable   => zeroEnable_d,
+				signExt      => signExt_d,
+				alu_a_muxCrtl=> alu_a_muxCrtl_d,
+				alu_b_muxCrtl=>alu_b_muxCrtl_d,
+				r7_enable	=> r7_enable_d,
+				memWrite_en  => memWrite_en_d,
+				beq_pc_crtl  => beq_pc_crtl_d,
+				rf_wren_mux  => rf_wren_mux _d,
+				rf_wren		=> rf_wren_d,
+				counter_mux  => counter_mux_d,
+				mem_mux		=> mem_mux_d,
+				rf_dataIn_sel=> rf_dataIn_sel_d,
+				alu_crtl     => alu_crtl_d,
+				op2in 	     => op2in _d);
 end mic;
